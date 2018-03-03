56e562781c38bae25020f4c728367c5d03a0a6c0
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2018-03-03 18:18:31 +0530

    application and oo python

--- Pythonapps/App5/backend.py
+++ Pythonapps/App5/backend.py
@@ -1,65 +1,51 @@
 import sqlite3
 
-def connect():
-    conn = sqlite3.connect("books.db")
-    cur_obj = conn.cursor()
-    cur_obj.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title TEXT, author TEXT,year INTEGER,isbn INTEGER)")
-    conn.commit()
-    conn.close()
+class Database:
 
-connect()
+    def __init__(self,db):
+        self.conn = sqlite3.connect(db)
+        self.cur_obj = self.conn.cursor()
+        self.cur_obj.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title TEXT, author TEXT,year INTEGER,isbn INTEGER)")
+        self.conn.commit()
 
-def insert(title,author,year,isbn):
-    conn = sqlite3.connect("books.db")
-    cur_obj = conn.cursor()
-    cur_obj.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",(title,author,year,isbn))
-    conn.commit()
-    conn.close()
+    def insert(self,title,author,year,isbn):
+        conn = sqlite3.connect("books.db")
+        cur_obj = conn.cursor()
+        cur_obj.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",(title,author,year,isbn))
+        conn.commit()
+        conn.close()
 
-def view():
-    conn = sqlite3.connect("books.db")
-    cur_obj = conn.cursor()
-    cur_obj.execute("SELECT * FROM book")
-    rows = cur_obj.fetchall()
-    conn.close()
-    return rows
+    def view(self):
+        self.cur_obj.execute("SELECT * FROM book")
+        rows = self.cur_obj.fetchall()
+        return rows
 
-def update(id,title,author,year,isbn):
-    conn = sqlite3.connect("books.db")
-    cur_obj = conn.cursor()
-    cur_obj.execute("UPDATE book SET title=?,author=?,year=?,isbn=? WHERE title=? OR author=? OR year=? OR isbn=? ",(title,author,year,isbn))
-    conn.commit()
-    conn.close()
+    def update(self,id,title,author,year,isbn):
+        self.cur_obj.execute("UPDATE book SET title=?,author=?,year=?,isbn=? WHERE id=? ",(title,author,year,isbn,id ))
+        self.conn.commit()
 
-def search(title="",author="",year="",isbn=""):
-    conn = sqlite3.connect("books.db")
-    cur_obj = conn.cursor()
-    cur_obj.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
-    rows = cur_obj.fetchall()
-    conn.commit()
-    conn.close()
-    return rows
+    def search(self,title="",author="",year="",isbn=""):
+        self.cur_obj.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+        rows = self.cur_obj.fetchall()
+        return rows
 
-def delete(id):
-    conn = sqlite3.connect("books.db")
-    cur_obj = conn.cursor()
-    cur_obj.execute("DELETE FROM book WHERE id=?",(id,))
-    conn.commit()
-    conn.close()
+    def delete(self,id):
+        self.cur_obj.execute("DELETE FROM book WHERE id=?",(id,))
+        self.conn.commit()
+    
+    def __del__(self):
+        self.conn.close()
 
 # insert("The deer","Ian Smith",2017,11293355843)
 # insert("The ghost","Brian Sands",2012,10293355843)
 # insert("The sweater","Liam Jones",2013,13293355843)
 # insert("The horse","Jon Frost",2010,12293355843)
 
-print(search(author="Ian Smith"))
+#print(search(author="Ian Smith"))
 #print(view())
 
-# delete(1)
-# delete(2)
-# delete(3)
-# delete(4)
-# delete(5)
+
+#delete(5)
 
 
 
--- Pythonapps/App5/bookStoreWindow.py
+++ Pythonapps/App5/bookStoreWindow.py
@@ -15,31 +15,50 @@ Close
 """
 
 from tkinter import *
-import backend 
+from backend import Database
 
+db = Database("books.db")
+
+def get_selected_row(event):
+    global selected_tuple
+    try:
+        index=list1.curselection()[0]
+        selected_tuple=list1.get(index)
+        title.delete(0,END)
+        title.insert(END,selected_tuple[1])
+        author.delete(0,END)
+        author.insert(END,selected_tuple[2])
+        year.delete(0,END)
+        year.insert(END,selected_tuple[3])
+        isbn.delete(0,END)
+        isbn.insert(END,selected_tuple[4])
+    except IndexError:
+        pass
 
 def view_command():
-    list1.delete("1.0",END)
-    for row in backend.view():
+    list1.delete(0,END)
+    for row in db.view():
         list1.insert(END,row)
 
 def search_command():
-    list1.delete("1.0",END)
-    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+    list1.delete(0,END)
+    for row in db.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
         list1.insert(END,row)
 
 def add_command():
-    backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
-        
+    db.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    list1.delete(0,END)
+    list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))    
 
 def update_command():
-    print()
+    db.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
 def delete_command():
-    print()
-def close_command():
-    print()
+    db.delete(selected_tuple[0])
+
 
 window = Tk()
+
+window.wm_title("Book Store")
 
 label_t = Label(window,text="Title")
 label_t.grid(row=0,column=0)
@@ -54,23 +73,23 @@ label_i = Label(window,text="ISBN")
 label_i.grid(row=1,column=2)
 
 title_text = StringVar()
-title=Entry(window)
-title.grid(row=0,column=1,textvariable=title_text)
+title=Entry(window,textvariable=title_text)
+title.grid(row=0,column=1)
 
 author_text = StringVar()
-author=Entry(window)
-author.grid(row=0,column=3,textvariable=author_text)
+author=Entry(window,textvariable=author_text)
+author.grid(row=0,column=3)
 
 year_text = StringVar()
-year=Entry(window)
-year.grid(row=1,column=1,textvariable=year_text)
+year=Entry(window,textvariable=year_text)
+year.grid(row=1,column=1)
 
 isbn_text = StringVar()
-isbn=Entry(window)
-isbn.grid(row=1,column=3,textvariable=isbn_text)
+isbn=Entry(window,textvariable=isbn_text)
+isbn.grid(row=1,column=3)
 
 
-list1=Text(window,height=8,width=45)
+list1=Listbox(window,height=8,width=45)
 list1.grid(row=2,column=0,rowspan=6,columnspan=2)
 
 sb1 = Scrollbar(window)
@@ -79,6 +98,7 @@ sb1.grid(row=2,column=2,rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+list1.bind('<<ListboxSelect>>',get_selected_row)
 
 view_all=Button(window,text="View All",width=12,command=view_command)
 view_all.grid(row=3,column=3)
@@ -95,7 +115,7 @@ update.grid(row=6,column=3)
 delete=Button(window,text="Delete",width=12,command=delete_command)
 delete.grid(row=7,column=3)
 
-close=Button(window,text="Close",width=12,command=close_command)
+close=Button(window,text="Close",width=12,command=window.destroy)
 close.grid(row=8,column=3)
 
 window.mainloop()
